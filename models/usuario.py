8e246d60646efba8c19f351fbadab17981909d9a
EstebanGarcesA <esteg254@gmail.com>
2026-07-31 13:37:35 -0500

    (feat): Configuración de la base de datos y el modelo usuario

---
+++ models/usuario.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+
+from database import Base
+
+class Usuario(Base):
+    __tablename__ = 'usuario'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, index=True)
+
+    nombre = Column(String(255),nullable=False )
+
+    contrasenha = Column(String(255),nullable=False)
+
+    email = Column(String(255),nullable=False)
+
+    fecha_registro = Column(DateTime,nullable=False)
+
