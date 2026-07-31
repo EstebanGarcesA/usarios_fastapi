85e379fa1d975fd7f35f4f34c2c5f8a2272b67ad
EstebanGarcesA <esteg254@gmail.com>
2026-07-31 13:52:07 -0500

    (feat): Esquema de validación

---
+++ schemas.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+
+
+class UsuarioBase(BaseModel):
+    nombre: str = Field(..., min_length=1,max_length=255, description="Nombre de usuario")
+    apellido: str = Field(..., min_length=1,max_length=255, description="Apellido de usuario")
+    contrasenha: str = Field(..., min_length=1,max_length=255, description="Contrasena de usuario")
+    email: str = Field(..., min_length=1,max_length=255, description="Email de usuario")
+
+class UsuarioCreate(UsuarioBase):
+    """
+    Esquema para crear un usuario
+    """
+    pass
+class UsuarioUpdate(UsuarioBase):
+    """
+    Esquema para actualizar un usuario
+    Permite que algunos campos sean opcionales
+    """
+
+    nombre: Optional[str] = Field(None, min_length = 1)
+    apellido: Optional[str] = Field(None, min_length = 1)
+    contrasenha: Optional[str] = Field(None, min_length = 1)
+    email: Optional[str] = Field(None, min_length = 1)
+    pass
+
+class UsuarioRead(BaseModel):
+    """"
+    Esquema para las respuestas del API.
+    Incluye el ID del Usuario
+    """
+    id: int
+    nombre: str
+    apellido: str
+    contrasenha: str
+    model_config = {
+        "from_attributes": True
+    }
